d0bf64aacc5b40a4e9cad6cf0c20b93c8b73a691
Ahmed Fathallah <nightelf_30@outlook.com>
2026-01-14 05:25:59 +0200

    feat: implement Recently Played section with dynamic playlist rendering; enhance PlayListCard component for improved visual presentation

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,71 @@
+// const playslistsArray = [
+//     {
+//         type: "PlayList",
+//         title: "Recently Played",
+//         songs: [
+//           {
+//             thumbnail: "https://placehold.co/600x400"
+//           }
+//         ]
+//     },
+// ];
+type Playlist = {
+    sectionTitle: string;
+    playlists: {
+        image: string;
+    }[];
+};
+const PlayListCard = ({ image }: { image: string }) => {
+    return (
+        <div className="rounded-md hover:cursor-pointer">
+            <img src={image} alt="Playlist" className="w-full h-full object-cover rounded-md" />
+        </div>
+    );
+};
 
+const PlayListsSection = ({ section }: { section: Playlist }) => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4">{section.sectionTitle}</h1>
+            <div className="flex flex-row gap-10">
+                {section.playlists.map((playlist) => (
+                    <PlayListCard key={playlist.image} image={playlist.image} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
+const recentlyPlayedSection = {
+    sectionTitle: "Recently Played",
+    playlists: [
+        {
+            image: "https://picsum.photos/600?random=1",
+        },
+        {
+            image: "https://picsum.photos/600?random=2",
+        },
+        {
+            image: "https://picsum.photos/600?random=3",
+        },
+        {
+            image: "https://picsum.photos/600?random=4",
+        },
+        {
+            image: "https://picsum.photos/600?random=5",
+        },
+        {
+            image: "https://picsum.photos/600?random=6",
+        },
+        {
+            image: "https://picsum.photos/600?random=7",
+        },
+    ],
+};
 export default function Test() {
-  return (
-    <div>
-      <h1 style={{ color: "red" }}>TEST PAGE</h1>
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-24">
+            <PlayListsSection section={recentlyPlayedSection} />
+        </div>
+    );
 }
